41407511e6ed17b527e4239cc68549522b01d201
fatonianggris <mfatonianggris@gmail.com>
2023-07-25 22:49:16 +0700

    repair sitekey

--- finance/dist/assets/js/pages/custom/login/transaction.js
+++ finance/dist/assets/js/pages/custom/login/transaction.js
@@ -1254,7 +1254,7 @@ $(document).ready(function () {
 				nama_siswa + "' (" + nis_siswa + ") dengan nominal Kredit (Rp. " + nominal + ") ? <br></br> <div id='recaptcha_delete'></div>",
 			didOpen: () => {
 				grecaptcha.render('recaptcha_delete', {
-					'sitekey': '6LcUwakcAAAAAFUZzySZs5K0ANRJIzbsq_NDVkdp'
+					'sitekey': '6LcUwakcAAAAAApKwPCj1lsgVOlmtv-uJzSuxyGG'
 				})
 			},
 			preConfirm: function () {
@@ -1332,7 +1332,7 @@ $(document).ready(function () {
 				nama_siswa + "' (" + nis_siswa + ") dengan nominal Debet (Rp. " + nominal + ") ? <br></br> <div id='recaptcha_delete'></div>",
 			didOpen: () => {
 				grecaptcha.render('recaptcha_delete', {
-					'sitekey': '6LcUwakcAAAAAFUZzySZs5K0ANRJIzbsq_NDVkdp'
+					'sitekey': '6LcUwakcAAAAAApKwPCj1lsgVOlmtv-uJzSuxyGG'
 				})
 			},
 			preConfirm: function () {
--- finance/dist/assets/js/pages/custom/login/transaction_recap.js
+++ finance/dist/assets/js/pages/custom/login/transaction_recap.js
@@ -1261,7 +1261,7 @@ $(document).ready(function () {
 				nama_siswa + "' (" + nis_siswa + ") dengan nominal Kredit (Rp. " + nominal + ") ? <br></br> <div id='recaptcha_delete'></div>",
 			didOpen: () => {
 				grecaptcha.render('recaptcha_delete', {
-					'sitekey': '6LcUwakcAAAAAFUZzySZs5K0ANRJIzbsq_NDVkdp'
+					'sitekey': '6LcUwakcAAAAAApKwPCj1lsgVOlmtv-uJzSuxyGG'
 				})
 			},
 			preConfirm: function () {
@@ -1339,7 +1339,7 @@ $(document).ready(function () {
 				nama_siswa + "' (" + nis_siswa + ") dengan nominal Debet (Rp. " + nominal + ") ? <br></br> <div id='recaptcha_delete'></div>",
 			didOpen: () => {
 				grecaptcha.render('recaptcha_delete', {
-					'sitekey': '6LcUwakcAAAAAFUZzySZs5K0ANRJIzbsq_NDVkdp'
+					'sitekey': '6LcUwakcAAAAAApKwPCj1lsgVOlmtv-uJzSuxyGG'
 				})
 			},
 			preConfirm: function () {
